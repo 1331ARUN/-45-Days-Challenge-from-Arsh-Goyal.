dda13be13a715c66cd6fc934e608b783fbc39623
1331ARUN <92286793+1331ARUN@users.noreply.github.com>
2023-08-21 11:45:50 +0530

    Added solution - LeetHub

---
+++ String formation from substring - GFG/string-formation-from-substring.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String input = read.readLine();
+            
+            Solution ob = new Solution();
+            int result = ob.isRepeat(input);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution 
+{
+    int isRepeat(String s)
+    {
+       
+       int n = s.length();
+      if(n==1){
+          return 0;
+      }
+        if(n==2){
+            if(s.charAt(0)==s.charAt(1)){
+            return 1;
+            }
+        else{
+            return 0;
+            }
+        }
+        String t = s+s;
+        String temp = t.substring(1,t.length()-1);
+        if(temp.indexOf(s)!=-1){
+            return 1;
+        }
+        return 0;
+    }
+}
+      
