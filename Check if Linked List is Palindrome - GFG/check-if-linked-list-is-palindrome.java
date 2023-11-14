c065edae2ae95a22e920c4f2ca4df4333db503bf
ARUN PRASAD <92286793+1331ARUN@users.noreply.github.com>
2023-11-14 16:39:13 +0530

    Added solution - LeetHub

---
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.java
@@ -0,0 +1,166 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinkedList_Palindrom
+{
+	 Node head;  
+	 Node tail;
+	
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		 
+		while(t>0)
+        {
+			int n = sc.nextInt();
+			//int k = sc.nextInt();
+			Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+			    tail.next = new Node(a);
+			    tail = tail.next;
+			}
+			
+			Solution g = new Solution();
+			if(g.isPalindrome(head) == true)
+			    System.out.println(1);
+		    else
+			    System.out.println(0);
+			t--;
+		}
+		
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class Solution
+{
+    //Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head) 
+    {
+        //Your code here
+        
+        if(head==null||head.next==null)
+        {
+            
+            return true;
+        }
+    
+       // Step 1: Find the middle of the linked list
+
+        Node slow=head;
+        Node fast=head;
+        
+        while(fast.next!=null&&fast.next.next!=null)
+        {
+            
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        // Step 2: Reverse the second half of the linked list
+        
+        slow.next=Reverse(slow.next);
+        
+       // Step 3: Compare the first half with the reversed second half
+
+Node firstHalf=head;
+Node secoundHalf=slow.next;
+
+while(secoundHalf!=null)
+{
+    
+    if(firstHalf.data!=secoundHalf.data)
+    {
+        
+        return  false;
+    }
+    
+    firstHalf=firstHalf.next;
+    secoundHalf=secoundHalf.next;
+}
+return true;
+}
+private static Node Reverse(Node head)
+{
+    Node prev=null;
+    Node curr=head;
+    Node next;
+    
+    while(curr!=null)
+    {
+        
+        next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
+}
+    
+
+
